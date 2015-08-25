ce0be4ffcbb00d20fc025e628da759d908418047
wyes3584371@gmail.com <student@302PC01.c302.tnfsh.tn.edu.tw>
2015-08-25 13:58:52 +0800

    add about page

---
+++ about.php
@@ -0,0 +1,17 @@
+<?php
+require('header.php');
+require("nav.php");
+?>
+<body>
+<div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-8 col-md-8 col-sm-8 trans_form_mh300 panel panel-default">
+			<center><h3>ABOUT</h3></center>
+			<h4>開發者:</h4><h3>大橋國中x三年四班x林宇翔</h3><br>
+			<h4>版面設計:</h4><h3>李旺陽</h3><br>
+			<h4>原始碼:</h4>https://github.com/dc1020090/dcjhurl
+			</div>
+		</div>
+</div>
+</body>
+</html>
