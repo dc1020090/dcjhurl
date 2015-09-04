d45250f00b5f4ad5785e8bd8307b784c1e33b44c
dc1020090 <wyes3584371@gmail.com>
2015-09-04 12:50:14 +0800

    update var.php

--- var.php
+++ var.php
@@ -1,5 +1,5 @@
 <?php
-$url="http://"   . $_SERVER['HTTP_HOST'] . '/' . basename(dirname(__FILE__)) . '/';
+$url="http://"   . $_SERVER['HTTP_HOST'] . '/' . basename(dirname(__FILE__)) . '/';//請勿更改
 
 $data="dcjh";
 ?>
